38da069518d9d716dd09a249dc887cd440b75ce0
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2020-07-27 10:38:41 -0400

    add cors support

--- epiviz-ws/app/main.py
+++ epiviz-ws/app/main.py
@@ -3,9 +3,20 @@
 from fastapi import FastAPI, Depends
 from app.db import database
 from app.workspaces import workspaces
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(openapi_url='/api/v1/openapi.json', docs_url='/api/v1/docs')
-        
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.on_event("startup")
 async def startup():
     await database.connect()
